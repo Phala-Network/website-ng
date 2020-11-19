7ba77fa91615e40c4eb2fb7d7eb33c279019ea20
limichange <limichange@hotmail.com>
2020-11-19 16:14:22 +0800

    update: i18n data of first screen

--- components/PageIndex/FirstScreen/index.tsx
+++ components/PageIndex/FirstScreen/index.tsx
@@ -7,7 +7,6 @@ import I18n from '../../I18n'
 type Props = {}
 
 const FirstScreen: React.FC<Props> = () => {
-  const description = 'Blockchain Confidentiality by Trusted Computing'
   const title = ['Phala Network', 'the pixalizer of', 'blockchain.']
 
   return (
@@ -18,10 +17,14 @@ const FirstScreen: React.FC<Props> = () => {
             return <div key={str}>{str}</div>
           })}
         </div>
-        <div className={styles.description}>{description}</div>
+        <div className={styles.description}>
+          <I18n
+            en='Blockchain Confidentiality by Trusted Computing'
+            zh='通过隐私计算帮助区块链保护秘密'></I18n>
+        </div>
         <div className={styles.button}>
           <Button color='primary'>
-            <I18n en='Learn about PHA' zh='开始学习 PHA'></I18n>
+            <I18n en='Learn about PHA' zh='了解 PHA'></I18n>
           </Button>
         </div>
       </div>
