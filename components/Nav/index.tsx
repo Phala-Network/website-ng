3b431da83b4270a4a7d669000e8e8c1f74fa2b6e
limichange <limichange@hotmail.com>
2020-11-19 14:25:28 +0800

    update: nav i18n name

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React from 'react'
+import I18n from '../I18n'
 import LangSwitch from '../LangSwitch'
 import Logo from '../Logo'
 import styles from './index.module.scss'
@@ -12,19 +13,28 @@ const Nav: React.FC<Props> = (props) => {
 
   const menus = [
     {
-      name: 'Learn',
+      name: {
+        en: 'Learn About PHA',
+        zh: '了解 PHA',
+      },
     },
     {
-      name: 'Developers',
+      name: {
+        en: 'Technology',
+        zh: '技术',
+      },
     },
     {
-      name: 'Ecosystem',
+      name: {
+        en: 'Ecosystem',
+        zh: '生态',
+      },
     },
     {
-      name: 'Products',
-    },
-    {
-      name: 'Resources',
+      name: {
+        en: 'Resources',
+        zh: '资源',
+      },
     },
   ]
 
@@ -39,8 +49,10 @@ const Nav: React.FC<Props> = (props) => {
         <ul>
           {menus.map((menu) => {
             return (
-              <li key={menu.name}>
-                <a href='#'>{menu.name}</a>
+              <li key={menu.name.en}>
+                <a href='#'>
+                  <I18n {...menu.name}></I18n>
+                </a>
               </li>
             )
           })}
