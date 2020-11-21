26adb746902a6d9182a558b75cd2d863b7bf7a9d
limichange <limichange@hotmail.com>
2020-11-21 18:54:33 +0800

    feature: add TimelineSectionConfig

---
+++ config/indexPage/TimelineSectionConfig.ts
@@ -0,0 +1,108 @@
+export const TimelineSectionConfig = {
+  doneTitle: { zh: '我们做了什么', en: "What we've done" },
+  doingTitle: {
+    en: 'What we’re doing',
+    zh: '我们正在做的',
+  },
+  timeline: {
+    done: [
+      {
+        year: 2018,
+        quarter: 4,
+        content: {
+          en: ['Phala Team was founded'],
+          zh: ['Phala Team was founded'],
+        },
+      },
+      {
+        year: 2019,
+        quarter: 2,
+        content: {
+          en: ['Built a strong community in China'],
+          zh: ['Built a strong community in China'],
+        },
+      },
+      {
+        year: 2019,
+        quarter: 3,
+        content: {
+          en: ['Whitepaper model', 'Web3 Foundation Grant for pLIBRA'],
+          zh: ['Whitepaper model', 'Web3 Foundation Grant for pLIBRA'],
+        },
+      },
+      {
+        year: 2019,
+        quarter: 4,
+        content: {
+          en: ['Hang Yin was entitled with Polkadot', 'Ambassador China'],
+          zh: ['Hang Yin was entitled with Polkadot', 'Ambassador China'],
+        },
+      },
+      {
+        year: 2020,
+        quarter: 1,
+        content: {
+          en: ['Economics model', 'Web3 Foundation Grant for W3A'],
+          zh: ['Economics model', 'Web3 Foundation Grant for W3A'],
+        },
+      },
+      {
+        year: 2020,
+        quarter: 2,
+        content: {
+          en: ['Testnet PoC-1', 'KSM x PHA Stakedrop'],
+          zh: ['Testnet PoC-1', 'KSM x PHA Stakedrop'],
+        },
+      },
+      {
+        year: 2020,
+        quarter: 3,
+        content: {
+          en: ['Testnet Rorschach', 'Kusama Darkpool development kickoff'],
+          zh: ['Testnet Rorschach', 'Kusama Darkpool development kickoff'],
+        },
+      },
+    ],
+    doing: [
+      {
+        year: 2020,
+        quarter: 4,
+        content: {
+          en: ['Testnet Vendetta', ' Canary launch: Darth Vader'],
+          zh: ['Testnet Vendetta', ' Canary launch: Darth Vader'],
+        },
+      },
+      {
+        year: 2021,
+        content: {
+          en: [
+            {
+              title: 'Kusama parachain auction',
+              text: [
+                'Launch Phala on Kusama',
+                'Decentralize Phala phase by phase',
+              ],
+            },
+            {
+              title: 'Polkadot parachain auction',
+              text: ['Launch Phala on Polkadot'],
+            },
+          ],
+          zh: [
+            {
+              title: 'Kusama parachain auction',
+              text: [
+                'Launch Phala on Kusama',
+                'Decentralize Phala phase by phase',
+              ],
+            },
+            {
+              title: 'Polkadot parachain auction',
+              text: ['Launch Phala on Polkadot'],
+            },
+          ],
+        },
+      },
+    ],
+  },
+}
