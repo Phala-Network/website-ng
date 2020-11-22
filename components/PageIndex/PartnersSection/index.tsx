67fda903f6a4f89466e623b0c9113209eff24d12
limichange <limichange@hotmail.com>
2020-11-22 09:15:23 +0800

    update: PartnersSection media style

--- components/PageIndex/PartnersSection/index.tsx
+++ components/PageIndex/PartnersSection/index.tsx
@@ -4,29 +4,51 @@ import SectionHeader from '../SectionHeader'
 import styles from './index.module.scss'
 import PartnerRow from './PartnerRow'
 import classnames from 'classnames'
+import { useBreakpoint } from '../../../hooks/useBreakpoint'
 
 type Props = {}
 
 const PartnersSection: React.FC<Props> = () => {
+  const { breakpoint } = useBreakpoint()
+
   return (
     <div className={styles.partnersSection}>
-      <div className='container-md'>
+      <div className='container'>
         <div className={classnames([styles.content, 'row'])}>
-          {/* partners */}
-          <div className={classnames([styles.partners, styles.xlHide])}>
-            <PartnerRow from={0} to={3}></PartnerRow>
-            <PartnerRow from={4} to={8}></PartnerRow>
-            <PartnerRow from={9} to={12}></PartnerRow>
-          </div>
+          {breakpoint === 'desktop' && (
+            <div className={styles.partners}>
+              <PartnerRow from={0} to={3}></PartnerRow>
+              <PartnerRow from={4} to={8}></PartnerRow>
+              <PartnerRow from={9} to={12}></PartnerRow>
+            </div>
+          )}
 
-          <div className={classnames([styles.partners, styles.xlShow])}>
-            <SectionHeader
-              className={classnames([styles.mobileHeader])}
-              title={PartnersSectionConfig.title}
-            />
+          {breakpoint === 'tablet' && (
+            <div className={styles.partners}>
+              <SectionHeader
+                className={classnames([styles.mobileHeader])}
+                title={PartnersSectionConfig.title}
+              />
 
-            <PartnerRow from={0} to={12}></PartnerRow>
-          </div>
+              <PartnerRow from={0} to={12}></PartnerRow>
+            </div>
+          )}
+
+          {breakpoint === 'mobile' && (
+            <div className={styles.partners}>
+              <SectionHeader
+                className={classnames([styles.mobileHeader])}
+                title={PartnersSectionConfig.title}
+              />
+
+              <PartnerRow from={0} to={1}></PartnerRow>
+              <PartnerRow from={2} to={3}></PartnerRow>
+              <PartnerRow from={4} to={6}></PartnerRow>
+              <PartnerRow from={7} to={8}></PartnerRow>
+              <PartnerRow from={9} to={10}></PartnerRow>
+              <PartnerRow from={11} to={12}></PartnerRow>
+            </div>
+          )}
 
           <SectionHeader
             className={classnames([styles.header])}
