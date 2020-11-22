a5e490cfef44403912592b27c7a378e1abdc2435
limichange <limichange@hotmail.com>
2020-11-22 09:36:35 +0800

    update: set row padding as 20px

--- components/PageIndex/ContractSection/index.tsx
+++ components/PageIndex/ContractSection/index.tsx
@@ -4,30 +4,29 @@ import Button from '../../Button'
 import SectionHeader from '../SectionHeader'
 import styles from './index.module.scss'
 import List from './List'
+import classnames from 'classnames'
 
 type Props = {}
 
 const ContractSection: React.FC<Props> = () => {
   return (
     <div className={styles.contractSection}>
-      <div className='container-lg'>
-        <div className={styles.content}>
-          <div className='row flex-column-reverse flex-lg-row '>
-            <div className='col-lg-6'>
-              <List></List>
-            </div>
-            <div className='col-lg-6'>
-              <div className={styles.introduce}>
-                <SectionHeader
-                  title={ContractSectionConfig.title}
-                  description={ContractSectionConfig.description}
-                />
+      <div className={classnames([styles.content, 'container-lg'])}>
+        <div className='row flex-column-reverse flex-lg-row '>
+          <div className='col-lg-6'>
+            <List></List>
+          </div>
+          <div className='col-lg-6'>
+            <div className={styles.introduce}>
+              <SectionHeader
+                title={ContractSectionConfig.title}
+                description={ContractSectionConfig.description}
+              />
 
-                <Button
-                  className={styles.button}
-                  text={ContractSectionConfig.button}
-                  hasArrowIcon></Button>
-              </div>
+              <Button
+                className={styles.button}
+                text={ContractSectionConfig.button}
+                hasArrowIcon></Button>
             </div>
           </div>
         </div>
