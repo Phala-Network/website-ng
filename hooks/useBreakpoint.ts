3c8f3cae1cab718c8cd5922c38924af958c6e625
limichange <limichange@hotmail.com>
2020-11-22 08:51:41 +0800

    update: set useBreakpoint desktop as 992

--- hooks/useBreakpoint.ts
+++ hooks/useBreakpoint.ts
@@ -1,6 +1,6 @@
 import _useBreakpoint from 'use-breakpoint'
 
-const BREAKPOINTS = { mobile: 0, tablet: 768, desktop: 1280 }
+const BREAKPOINTS = { mobile: 0, tablet: 768, desktop: 992 }
 
 export function useBreakpoint() {
   return _useBreakpoint(BREAKPOINTS, 'desktop')
