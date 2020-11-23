90ea42edfe79f55d0bccf3c084d6b410f9c199b2
limichange <limichange@hotmail.com>
2020-11-23 17:26:46 +0800

    update:  PageMiner add buttons

--- components/PageMiner/index.tsx
+++ components/PageMiner/index.tsx
@@ -4,6 +4,10 @@ import NormalLayout from '../NormalLayout'
 import styles from './index.module.scss'
 import DetailPageHeader from '../DetailPageHeader'
 import PageTitle from '../PageTitle'
+import DetailPageHeaderButtons from '../DetailPageHeaderButtons'
+import DetailPageButton from '../DetailPageButton'
+import { WikiConfig } from '../../config/WikiConfig'
+import { EconomyWhitepaperConfig } from '../../config/EconomyWhitepaperConfig'
 
 type Props = {}
 
@@ -15,6 +19,29 @@ const PageMiner: React.FC<Props> = () => {
       <NormalLayout className={styles.pageMiner}>
         <div className='container'>
           <DetailPageHeader {...PageMinerConfig}></DetailPageHeader>
+
+          <DetailPageHeaderButtons>
+            <DetailPageButton
+              type='link'
+              href={'https://wiki.phala.network/en-us/docs/poc3/'}
+              text={{
+                en: 'Mining Tutorial',
+                zh: '挖坑教程',
+              }}
+            />
+
+            <DetailPageButton
+              type='link'
+              href={WikiConfig.href}
+              text={WikiConfig.name}
+            />
+
+            <DetailPageButton
+              type='link'
+              href={EconomyWhitepaperConfig.href}
+              text={EconomyWhitepaperConfig.name}
+            />
+          </DetailPageHeaderButtons>
         </div>
       </NormalLayout>
     </>
