fc1c8998911dd859e2ddf817aec4c78a1e11c34a
limichange <limichange@hotmail.com>
2020-11-24 11:23:16 +0800

    update: timeline section i18n config

--- config/pageIndex/TimelineSectionConfig.ts
+++ config/pageIndex/TimelineSectionConfig.ts
@@ -1,8 +1,8 @@
 export const TimelineSectionConfig = {
-  doneTitle: { zh: '我们做了什么', en: "What we've done" },
+  doneTitle: { zh: '已完成', en: "What we've done" },
   doingTitle: {
     en: 'What we’re doing',
-    zh: '我们正在做的',
+    zh: '进行中',
   },
   timeline: {
     done: [
@@ -11,7 +11,7 @@ export const TimelineSectionConfig = {
         quarter: 4,
         content: {
           en: ['Phala Team was founded'],
-          zh: ['Phala 团队成立'],
+          zh: ['Phala团队成立，确定项目方向'],
         },
       },
       {
@@ -19,7 +19,7 @@ export const TimelineSectionConfig = {
         quarter: 2,
         content: {
           en: ['Built a strong community in China'],
-          zh: ['建立了强大的中国社区'],
+          zh: ['在中国开始建立社区'],
         },
       },
       {
@@ -27,7 +27,10 @@ export const TimelineSectionConfig = {
         quarter: 3,
         content: {
           en: ['Whitepaper model', 'Web3 Foundation Grant for pLIBRA'],
-          zh: ['Whitepaper model', 'Web3 Foundation Grant for pLIBRA'],
+          zh: [
+            '发布技术白皮书',
+            'Phala 旗下首个产品 pLIBRA 获得 Web3 基金会 Grant 奖金',
+          ],
         },
       },
       {
@@ -35,7 +38,7 @@ export const TimelineSectionConfig = {
         quarter: 4,
         content: {
           en: ['Hang Yin was entitled with Polkadot', 'Ambassador China'],
-          zh: ['Hang Yin was entitled with Polkadot', 'Ambassador China'],
+          zh: ['尹航成为波卡中国大使'],
         },
       },
       {
@@ -43,7 +46,7 @@ export const TimelineSectionConfig = {
         quarter: 1,
         content: {
           en: ['Economics model', 'Web3 Foundation Grant for W3A'],
-          zh: ['Economics model', 'Web3 Foundation Grant for W3A'],
+          zh: ['发布经济白皮书', '通过Web3 Analytics再次获得获得Grant资助'],
         },
       },
       {
@@ -51,7 +54,7 @@ export const TimelineSectionConfig = {
         quarter: 2,
         content: {
           en: ['Testnet PoC-1', 'KSM x PHA Stakedrop'],
-          zh: ['Testnet PoC-1', 'KSM x PHA Stakedrop'],
+          zh: ['Phala POC-1 测试网上线', '完成“抵押空投”活动'],
         },
       },
       {
@@ -59,7 +62,10 @@ export const TimelineSectionConfig = {
         quarter: 3,
         content: {
           en: ['Testnet Rorschach', 'Kusama Darkpool development kickoff'],
-          zh: ['Testnet Rorschach', 'Kusama Darkpool development kickoff'],
+          zh: [
+            'Phala POC-2 “罗夏测试网” 上线',
+            '受Kusama社区委托，与社区合作开发去中心化暗池协议',
+          ],
         },
       },
     ],
@@ -69,7 +75,10 @@ export const TimelineSectionConfig = {
         quarter: 4,
         content: {
           en: ['Testnet Vendetta', ' Canary launch: Darth Vader'],
-          zh: ['Testnet Vendetta', ' Canary launch: Darth Vader'],
+          zh: [
+            '上线 Phala POC-3 “Vendetta测试网”',
+            '上线 预备主网 Darth Vader',
+          ],
         },
       },
       {
@@ -91,15 +100,18 @@ export const TimelineSectionConfig = {
           ],
           zh: [
             {
-              title: 'Kusama parachain auction',
+              title: '第一批Kusama Slot Auction 事件后',
               text: [
-                'Launch Phala on Kusama',
-                'Decentralize Phala phase by phase',
+                'Phala Network成为Kusama平行链',
+                '从POA(权限化管理)到去中心化状态过度',
               ],
             },
             {
-              title: 'Polkadot parachain auction',
-              text: ['Launch Phala on Polkadot'],
+              title: '第一批Polkadot Slot Auction 事件后',
+              text: [
+                'Phala Network成为Polkadot平行链',
+                '从Kusama平行链迁移至Polkadot平行链',
+              ],
             },
           ],
         },
