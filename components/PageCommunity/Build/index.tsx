6812f1f9af56ad288c9e20c82b8d39941410a289
limichange <limichange@hotmail.com>
2020-11-25 20:32:18 +0800

    update: PageCommunity use TextCardList

--- components/PageCommunity/Build/index.tsx
+++ components/PageCommunity/Build/index.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import Card, { CardProps } from '../../Card'
 import I18n from '../../I18n'
+import CardList from '../../TextCardList'
 import styles from './index.module.scss'
 
 const config = [
@@ -33,29 +34,12 @@ const config = [
   },
 ]
 
+const imgPath = '/images/PageCommunity/icon-build-'
+
 const Build: React.FC<CardProps> = (props) => {
   return (
     <Card type='small' cardContentClassName={styles.cardContent} {...props}>
-      {config.map((item, index) => {
-        return (
-          <div className={styles.item}>
-            <Link passHref href={item.url}>
-              <a target='_blank'>
-                <img
-                  src={`/images/PageCommunity/icon-build-${index}.svg`}
-                  alt={item.url}
-                />
-                <div className={styles.title}>
-                  <I18n {...item.title}></I18n>
-                </div>
-                <div className={styles.description}>
-                  <I18n {...item.description}></I18n>
-                </div>
-              </a>
-            </Link>
-          </div>
-        )
-      })}
+      <CardList imgPath={imgPath} config={config}></CardList>
     </Card>
   )
 }
