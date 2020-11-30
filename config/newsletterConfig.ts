05e787e3a8fb896ad65abd70fd86a9d1d58f8363
limichange <limichange@hotmail.com>
2020-11-30 15:17:25 +0800

    update: newslette title

--- config/newsletterConfig.ts
+++ config/newsletterConfig.ts
@@ -6,8 +6,9 @@ export const newsletterConfig = [
       zh: 'https://mp.weixin.qq.com/s/83xA-lBG56a7xQZfYwq7ig',
     },
     news: {
-      en: '1,000,000 PHA: Announcing 1605 Race, GKOL Workshop, and Bounties',
-      zh: '1,000,000 PHA：1605竞赛与Vendetta激励计划将于11月27日启动',
+      en:
+        '🔥1,000,000 PHA: Announcing 1605 Race, GKOL Workshop, and Bounties🔥',
+      zh: '🔥1,000,000 PHA：1605竞赛与Vendetta激励计划将于11月27日启动🔥',
     },
   },
 ]
