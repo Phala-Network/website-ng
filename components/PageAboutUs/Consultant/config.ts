9ad529aad71e7e725603cc188bd8115f5f5776a7
jasl <jasl9187@hotmail.com>
2021-05-03 05:29:24 +0800

    Update Sandro bio

--- components/PageAboutUs/Consultant/config.ts
+++ components/PageAboutUs/Consultant/config.ts
@@ -1,22 +1,5 @@
 export const config = [
   {
-    avatar: 'people11',
-    name: {
-      en: 'Sandro Gorduladze',
-      zh: 'Sandro Gorduladze',
-    },
-    position: {
-      en: 'Advisor',
-      zh: '顾问',
-    },
-    description: {
-      en:
-        'Sandro Gorduladze is the CIO at Gagra Ventures, an early-stage investment firm. Prior to launching the fund, Sandro ran Research at HASH CIB, a digital asset financial services company that made a name for itself with in-depth research reports. Prior to joining Hash Sandro worked at PwC Russia, advising TMT sector companies on tax.',
-      zh:
-        'Sandro Gorduladze是HASH CIB——一家主要覆盖EMEA和亚洲地区的数字资产投资公司——的天使投资人和合伙人。 Sandro在HASH设立了研究部门，该部门因其深入的报告而声名大噪。 加入HASH之前，Sandro曾在俄罗斯普华永道工作，为TMT行业的公司提供税务咨询。',
-    },
-  },
-  {
     avatar: 'people5',
     name: {
       en: 'Dr. Shunfan Zhou',
@@ -48,6 +31,23 @@ export const config = [
         'Ph.D., Economics, Toulouse School of Economics. Led the game-theoretic analysis of Phala Network token economics model.',
       zh:
         '法国图卢兹大学经济学博士。主导了 Phala Network 经济设计的博弈论分析工作。',
+    },
+  },
+  {
+    avatar: 'people11',
+    name: {
+      en: 'Sandro Gorduladze',
+      zh: 'Sandro Gorduladze',
+    },
+    position: {
+      en: 'Advisor',
+      zh: '顾问',
+    },
+    description: {
+      en:
+        'Sandro Gorduladze is the CIO at Gagra Ventures, an early-stage investment firm. Prior to launching the fund, Sandro ran Research at HASH CIB, a digital asset financial services company that made a name for itself with in-depth research reports. Prior to joining Hash Sandro worked at PwC Russia, advising TMT sector companies on tax.',
+      zh:
+        'Sandro Gorduladze是HASH CIB——一家主要覆盖EMEA和亚洲地区的数字资产投资公司——的天使投资人和合伙人。 Sandro在HASH设立了研究部门，该部门因其深入的报告而声名大噪。 加入HASH之前，Sandro曾在俄罗斯普华永道工作，为TMT行业的公司提供税务咨询。',
     },
   },
 ]
