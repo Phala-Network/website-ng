ed39bf2e342d54333d8daec971739a35b667bb63
limichange <limichange@hotmail.com>
2021-06-16 17:41:06 +0800

    update: newsletterConfig

--- src/config/newsletterConfig.ts
+++ src/config/newsletterConfig.ts
@@ -3,12 +3,13 @@ export const newsletterConfig = [
     link: {
       en:
         'https://medium.com/phala-network/phala-transparent-and-private-global-computation-cloud-2d80c70ad1e9',
-      zh: 'https://mp.weixin.qq.com/s/duidJdYQgzudCnGqDKtXSg'
+      zh: 'https://mp.weixin.qq.com/s/L5ptBh01zSkkD88g8bO2xw'
     },
     news: {
       en:
-        'Phala Network: Transparent and Private Global Computation Cloud in Web3.0',
-      zh: '一文读懂Phala Network——web3.0 时代的隐私计算云平台'
+        'https://medium.com/phala-network/khala-crowdloan-dapp-live-staking-now-fe02aae2a079',
+      zh:
+        'Khala Crowdloan Dapp 已上线 ，携好友质押 KSM 助力 Kusama 插槽竞拍，享150万 PHA 奖励！'
     }
   }
 ]
