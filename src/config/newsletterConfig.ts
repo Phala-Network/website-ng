17110946b8ab4794478d4ff64994a29bce74d6f0
limichange <limichange@hotmail.com>
2021-06-16 18:54:44 +0800

    update: newsletterConfig

--- src/config/newsletterConfig.ts
+++ src/config/newsletterConfig.ts
@@ -9,7 +9,7 @@ export const newsletterConfig = [
       en:
         'Khala Crowdloan Dapp is available, contribute KSM for Khala, share 15 Million PHA with your friends!',
       zh:
-        'Khala Crowdloan Dapp 已上线 ，携好友质押 KSM 助力 Kusama 插槽竞拍，享150万 PHA 奖励！'
+        'Khala Crowdloan Dapp 已上线 ，携好友质押 KSM 助力 Kusama 插槽竞拍，享1500万 PHA 奖励！'
     }
   }
 ]
