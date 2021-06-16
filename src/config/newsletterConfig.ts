3a10eeff598a4a673d77067d6a0dd1686368a584
limichange <limichange@hotmail.com>
2021-06-16 17:59:22 +0800

    update: newsletterConfig

--- src/config/newsletterConfig.ts
+++ src/config/newsletterConfig.ts
@@ -2,12 +2,12 @@ export const newsletterConfig = [
   {
     link: {
       en:
-        'https://medium.com/phala-network/phala-transparent-and-private-global-computation-cloud-2d80c70ad1e9',
+        'https://medium.com/phala-network/khala-crowdloan-dapp-live-staking-now-fe02aae2a079',
       zh: 'https://mp.weixin.qq.com/s/L5ptBh01zSkkD88g8bO2xw'
     },
     news: {
       en:
-        'https://medium.com/phala-network/khala-crowdloan-dapp-live-staking-now-fe02aae2a079',
+        'Khala Crowdloan Dapp is available, contribute KSM for Khala, share 15 Million PHA with your friends!',
       zh:
         'Khala Crowdloan Dapp 已上线 ，携好友质押 KSM 助力 Kusama 插槽竞拍，享150万 PHA 奖励！'
     }
