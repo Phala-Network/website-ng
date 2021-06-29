0ccc2ec11da7f8b4ddc5e5b44caad8da337c7724
limichange <limichange@hotmail.com>
2021-06-29 14:31:48 +0800

    feat: player scroll animation

--- src/components/PageIndex/CloudSection/PlayerContent/index.tsx
+++ src/components/PageIndex/CloudSection/PlayerContent/index.tsx
@@ -8,9 +8,12 @@ gsap.registerPlugin(ScrollTrigger)
 type Props = {}
 
 const PlayerContent: React.FC<Props> = () => {
-  const section = useRef<HTMLBodyElement>(null)
+  const section = useRef<HTMLDivElement>(null)
   const leftHandClass = `.${styles.leftHand}`
   const rightHandClass = `.${styles.rightHand}`
+  const playerButtonClass = `.${styles.playerButton}`
+  const browserClass = `.${styles.browser}`
+  const blocksClass = `.${styles.blocks}`
 
   useEffect(() => {
     if (!section.current) return
@@ -18,26 +21,38 @@ const PlayerContent: React.FC<Props> = () => {
     const sectionDom = section.current
     const leftHandDom = sectionDom.querySelector(leftHandClass)
     const rightHandDom = sectionDom.querySelector(rightHandClass)
+    const playerButtonDom = sectionDom.querySelector(playerButtonClass)
+    const browserDom = sectionDom.querySelector(browserClass)
+    const blocksDom = sectionDom.querySelector(blocksClass)
 
-    const enterTimeline = gsap.timeline({
+    const timeline = gsap.timeline({
       scrollTrigger: {
         trigger: sectionDom,
         start: '30% 70%',
         end: '70% 30%',
-        scrub: true,
-        markers: true
+        scrub: true
       }
     })
 
-    enterTimeline.to(leftHandDom, { x: 200, opacity: 1, scale: 1.3 }, 0)
-    enterTimeline.to(leftHandDom, { x: 0, opacity: 0.6, scale: 1 }, 1)
+    timeline.to(blocksDom, { opacity: 1 }, 0)
+    timeline.to(blocksDom, { opacity: 0.5 }, 0.6)
 
-    enterTimeline.to(rightHandDom, { x: -200, opacity: 1, scale: 1.3 }, 0)
-    enterTimeline.to(rightHandDom, { x: 0, opacity: 0.6, scale: 1 }, 1)
+    timeline.to(browserDom, { opacity: 0 }, 0)
+    timeline.to(browserDom, { opacity: 1 }, 0.6)
+    timeline.to(browserDom, { opacity: 0 }, 1)
+
+    timeline.to(playerButtonDom, { opacity: 1, scale: 1.8 }, 0)
+    timeline.to(playerButtonDom, { opacity: 0.5, scale: 1 }, 1)
+
+    timeline.to(leftHandDom, { x: 100, opacity: 1, scale: 1.4 }, 0)
+    timeline.to(leftHandDom, { x: 0, opacity: 0.6, scale: 1 }, 1)
+
+    timeline.to(rightHandDom, { x: -100, opacity: 1, scale: 1.4 }, 0)
+    timeline.to(rightHandDom, { x: 0, opacity: 0.6, scale: 1 }, 1)
 
     return () => {
-      enterTimeline.kill()
-      enterTimeline.clear(true)
+      timeline.kill()
+      timeline.clear(true)
     }
   }, [])
 
@@ -45,6 +60,9 @@ const PlayerContent: React.FC<Props> = () => {
     <section ref={section} className={styles.comparisonSection}>
       <div className={styles.leftHand}></div>
       <div className={styles.rightHand}></div>
+      <div className={styles.blocks}></div>
+      <div className={styles.browser}></div>
+      <div className={styles.playerButton}></div>
     </section>
   )
 }
