d2bf8b3074b50b3e615f26e45fcd981c2a20b2ce
limichange <limichange@hotmail.com>
2021-08-17 15:16:04 +0800

    feat: update links & text

--- src/config/newsletterConfig.ts
+++ src/config/newsletterConfig.ts
@@ -1,15 +1,12 @@
 export const newsletterConfig = [
   {
     link: {
-      en:
-        'https://wiki.phala.network/en-us/docs/khala-mining/',
+      en: 'https://wiki.phala.network/en-us/docs/khala-mining/',
       zh: 'https://wiki.phala.network/zh-cn/docs/khala-mining/'
     },
     news: {
-      en:
-        'Para-1 testnet goes live,TEE mining test starts!',
-      zh:
-        'Para-1 测试网上线，全新经济模型及挖矿机制，欢迎体验！'
+      en: 'Para-1 testnet goes live, TEE mining test starts!',
+      zh: 'Para-1 测试网上线，全新经济模型及挖矿机制，欢迎体验！'
     }
   },
   {
