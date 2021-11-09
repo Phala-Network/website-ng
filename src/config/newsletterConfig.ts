f8003a20c18a331e534d631f51ebc6051019bc26
limichange <limichange@hotmail.com>
2021-11-09 14:58:37 +0800

    feat: update newsletterConfig

--- src/config/newsletterConfig.ts
+++ src/config/newsletterConfig.ts
@@ -1,22 +1,13 @@
 export const newsletterConfig = [
   {
     link: {
-      en: 'https://wiki.phala.network/en-us/docs/khala-mining/',
-      zh: 'https://wiki.phala.network/zh-cn/docs/khala-mining/'
+      en: 'https://forum.phala.network/t/topic/3213',
+      zh: 'https://forum.phala.network/t/topic/3213'
     },
     news: {
-      en: '🔥🔥🔥Khala Secure Worker Mining Starts! TRY NOW！',
-      zh: '🔥🔥🔥Khala Secure Worker 挖矿正式上线，阅读指南，立刻加入！'
-    }
-  },
-  {
-    link: {
-      en: 'https://app.phala.network/delegate/',
-      zh: 'https://app.phala.network/delegate/'
-    },
-    news: {
-      en: 'Be a delegator, share mining rewards!💰',
-      zh: '成为委托人，参与链上挖矿收益分配💰'
+      en:
+        '🎤 Open discussion: Polkadot slot auction strategy for Phala Network!',
+      zh: '🎤 社区开放讨论：Phala 波卡卡槽竞拍策略交流'
     }
   }
 ]
