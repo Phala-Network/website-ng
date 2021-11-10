b745d50f6d9a58778515e41c30adcf58d6ce4f94
limichange <limichange@hotmail.com>
2021-11-10 15:54:03 +0800

    fix: FirstScreen type error

--- src/components/PageIndex/FirstScreen/index.tsx
+++ src/components/PageIndex/FirstScreen/index.tsx
@@ -1,13 +1,11 @@
 import { ExplorerConfig, PhalaAppConfig } from '../../../config'
 import Button from '../../Button'
 import I18n from '../../I18n'
-import React from 'react'
+import React, { FC } from 'react'
 import classnames from 'classnames'
 import * as styles from './index.module.scss'
 
-interface Props {}
-
-const FirstScreen = (x: Props) => {
+const FirstScreen: FC = () => {
   const title = {
     en: ['Global', 'Confidential', 'Compute Cloud'],
     zh: ['Phala Network', '给区块链打上马赛克']
@@ -18,27 +16,33 @@ const FirstScreen = (x: Props) => {
       <div className={styles.bg}></div>
       <div className={classnames(['container', styles.content])}>
         <div className={styles.title}>
-          <I18n render={(str: string) => <div key={str}>{str}</div>} {...title}></I18n>
+          <I18n
+            render={(str: string | string[]) => (
+              <div key={str.toString()}>{str}</div>
+            )}
+            {...title}
+          />
         </div>
         <div className={styles.description}>
           <I18n
-            en='Blockchain Confidentiality by Trusted Computing'
-            zh='使用隐私计算保卫区块链上秘密'></I18n>
+            en="Blockchain Confidentiality by Trusted Computing"
+            zh="使用隐私计算保卫区块链上秘密"
+          />
         </div>
         <div className={styles.buttons}>
           <Button
             text={PhalaAppConfig.name}
             href={PhalaAppConfig.href}
             className={styles.button}
-            type='link'
-            color='primary'></Button>
+            type="link"
+            color="primary"></Button>
           <Button
             text={ExplorerConfig.name}
             href={ExplorerConfig.href}
             className={styles.button}
             hasArrowIcon
-            type='link'
-            color='gray'></Button>
+            type="link"
+            color="gray"></Button>
         </div>
       </div>
     </section>
