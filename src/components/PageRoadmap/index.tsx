8b6e9ba54d18e65ca9176692397291de06cf8ad4
lucasliu <120998498+lucasliu2022@users.noreply.github.com>
2023-03-02 17:54:59 +0800

    change locale

--- src/components/PageRoadmap/index.tsx
+++ src/components/PageRoadmap/index.tsx
@@ -2,12 +2,14 @@ import * as styles from './index.module.scss'
 import React from 'react'
 import EnRoadMap from './EnRoadMap'
 import CnRoadMap from './CnRoadMap'
+import { useLocale } from '../../hooks/useLocale'
 
 export default function PageRoadMap() {
+  const locale = useLocale()
+  
   return (
     <div className={styles.container}>
-      {/* <EnRoadMap /> */}
-      <CnRoadMap />
+      {locale === 'en' ? <EnRoadMap /> : <CnRoadMap />}
     </div>
   )
 }
