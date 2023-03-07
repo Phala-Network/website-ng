798c88c324ec87d07b436d9db48fb46fd0a9c17f
lucasliu <120998498+lucasliu2022@users.noreply.github.com>
2023-03-07 16:36:15 +0800

    add header and footer

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -1,27 +1,31 @@
 import { Text } from '../NotionBlock/Text'
 import { renderBlock } from '../NotionBlock/block'
 import React, { Fragment } from 'react'
+import * as styles from './index.module.scss'
+import NormalLayout from '../NormalLayout'
 
-export default function Blog ({ pageContext }: any) {
+export default function Blog({ pageContext }: any) {
   const blog = pageContext.data
 
   return (
-    <>
-      <h1>
-        <Text text={blog.title} />
-      </h1>
-      <div>
-        <img
-          style={{ verticalAlign: 'middle', width: '100%' }}
-          src={blog.headlineImg}
-          alt=""
-        />
+    <NormalLayout>
+      <div className={styles.container}>
+        <h1>
+          <Text text={blog.title} />
+        </h1>
+        <div>
+          <img
+            style={{ verticalAlign: 'middle', width: '100%' }}
+            src={blog.headlineImg}
+            alt=""
+          />
+        </div>
+        <article style={{ padding: '20px' }}>
+          {blog?.content?.map((block: any) => {
+            return <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+          }) || null}
+        </article>
       </div>
-      <article style={{ padding: '20px' }}>
-        {blog?.content?.map((block: any) => {
-          return <Fragment key={block.id}>{renderBlock(block)}</Fragment>
-        }) || null}
-      </article>
-    </>
+    </NormalLayout>
   )
 }
--- src/components/BlogList/index.tsx
+++ src/components/BlogList/index.tsx
@@ -1,20 +1,24 @@
 import { Text } from '../NotionBlock/Text'
 import React from 'react'
+import NormalLayout from '../NormalLayout'
+import * as styles from './index.module.scss'
 
-export default function BlogList ({ pageContext }: any) {
+export default function BlogList({ pageContext }: any) {
   const data = pageContext.data
 
   return (
-    <div>
-      {data.map((d: any) => {
-        return (
-          <div key={d.id}>
-            <a href={`/blog${d.url}`}>
-              <Text text={d.title} />
-            </a>
-          </div>
-        )
-      })}
-    </div>
+    <NormalLayout>
+      <div className={styles.container}>
+        {data.map((d: any) => {
+          return (
+            <div key={d.id}>
+              <a href={`/blog${d.url}`}>
+                <Text text={d.title} />
+              </a>
+            </div>
+          )
+        })}
+      </div>
+    </NormalLayout>
   )
 }
